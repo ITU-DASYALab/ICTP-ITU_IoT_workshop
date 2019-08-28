c3a0d52f3f0da3ad22b475e40a3ea3cc2dd7b183
sbut <sbut@itu.dk>
2019-08-28 07:40:35 +0200

    merged Rwanda and Pune versions

--- Code/wifi/WPA/main.py
+++ Code/wifi/WPA/main.py
@@ -4,9 +4,9 @@ wlan = WLAN(mode=WLAN.STA)
 
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'MarconiLab':
+    if net.ssid == '__YOUR_SSID__':
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'marconi-lab'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, '__YOUR_PW__'), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
